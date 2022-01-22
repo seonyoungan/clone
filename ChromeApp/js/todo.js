4d109c3e07dabdcbd6bec2c7f5a4557bd2313b07
seonyoungan <asy093@naver.com>
2022-01-22 23:52:46 +0900

    update todolist

--- ChromeApp/js/todo.js
+++ ChromeApp/js/todo.js
@@ -5,12 +5,12 @@ const toDoList = document.getElementById("todo-list");
 function deleteToDo(event){
     //지금 우리가 삭제해야할 것은 li니까 event.target.parentElement까지 가야하는 것이다.
     const li = event.target.parentElement; //target : element의 속성, parentElemnent : 이걸 해야 누구를 삭제해야하는지 알 수 있게 되는거지
-    li.remove(); //삭제!
+    li.remove(); //(html(=보여지는부분)에서 삭제! db는 따로 삭제해야함)
 }
 
 //localstorage에 저장해보기
 TODOS_KEY = "Todos";
-const toDos = [];
+let toDos = []; //const로 선언 : 어플시작시 toDos는 항상 비워져있음.  / let으로 하면 업데이트가 됨
 function saveToDos(){
     localStorage.setItem(TODOS_KEY,JSON.stringify(toDos));
 } 
@@ -21,7 +21,7 @@ JSON.stringify(toDos) : text형태로 저장할 수 있나봐
 function paintToDo(newTodo){
     const li = document.createElement("li"); //변수명은 상관없으나 createElement("li")는 꼭 이렇게 써야댄당
     const span = document.createElement("span"); //span 기능 가져오깅
-    span.innerText = newTodo;
+    span.innerText = newTodo.text;
     const button = document.createElement("button") //버튼 기능 가져오깅 (event와 수신하게끔)
     button.innerText = "❌";
     button.addEventListener("click", deleteToDo);
@@ -34,11 +34,18 @@ function paintToDo(newTodo){
 function handleToDoSubmit(event){
     event.preventDefault(); //enter를 쳐도 새로고침이 되지 않도록 만들기 (greeting에서도 했다)
     const newTodo = toDoInput.value; 
-    toDoInput.value = "";
+
     /*toDoInput.value;를 비워주자.
     왜? toDoInput.value;를 -> newTodo라는 변수에 복사하는 거니까!*/
-    toDos.push(newTodo); //
-    paintToDo(newTodo); //paintToDo를 호출한 후 newTodo에 보내는 것임
+    toDoInput.value = "";
+
+    const newTodoObj = {
+        text:newTodo,
+        id:Date.now()
+    }; //storage에서 {text : "어쩌구" id : "12354435"} 식으로 저장되도록 object생성
+    toDos.push(newTodoObj); //
+    
+    paintToDo(newTodoObj); //paintToDo를 호출한 후 newTodo에 보내는 것임 text가 아니라 object로..
     saveToDos();
 }
 
@@ -55,10 +62,10 @@ toDoForm.addEventListener("submit",handleToDoSubmit);
 
 const savedToDos = localStorage.getItem(TODOS_KEY);
 if(savedToDos !== null){
-    //로컬스토리지에서 가져온 string을 살아있는 JS object로 만들기
+    //로컬스토리지에서 가져온 string을 살아있는 JS object로 만들기 (.복원)
     const parsedToDos = JSON.parse(savedToDos);
-
+    
+    toDos = parsedToDos; // let toDos = [];를 한 후  toDos에 가져오기
     //array 내에 있는 item들을 각각 다르게 활용(function)하고 싶을 경우 어떻게 해야할까?
-    console.log(parsedToDos);
-    parsedToDos.forEach((item) => console.log("this is the turn of", item));
+    parsedToDos.forEach(paintToDo);
 }
