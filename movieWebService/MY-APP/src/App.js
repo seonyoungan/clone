8c75ca894714c4cfed2b5eb22bc003f1e153e3c5
안선영 <74126735+seonyoungan@users.noreply.github.com>
2022-02-04 15:39:38 +0900

    create-MY-APP

---
+++ movieWebService/MY-APP/src/App.js
@@ -0,0 +1,13 @@
+
+import Button from "./Button";
+
+function App() {
+  return (
+    <div>
+      <h1>welcome back!</h1>
+      <Button text={"continue"} />
+    </div>
+  );
+}
+
+export default App;
